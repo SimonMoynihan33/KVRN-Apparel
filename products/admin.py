8c40c2f293e03e6bb9b36c6fdf6ecf9ca622f96c
Simon Moynihan <simon.moynihan33@gmail.com>
2024-10-15 15:01:51 +0000

    Add docstrings to views and link products in main-nav and index.html

--- products/admin.py
+++ products/admin.py
@@ -4,12 +4,19 @@ from .models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """
+    Register and extend admin panel for Categories
+    """
     list_display = ('friendly_name', 'name', 'gender')  # Add gender to the list display
     list_filter = ('gender',)
+
 
 # Customize the Product admin interface
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """
+    Register and extend admin panel for Products
+    """
     list_display = (
         'sku',
         'name',
@@ -24,5 +31,9 @@ class ProductAdmin(admin.ModelAdmin):
     filter_horizontal = ('categories',)
 
     def get_categories(self, obj):
+        """
+        Returns a comma-separated list of category names for a product in the admin interface.
+        Used in the ProductAdmin list_display to handle the Many-to-Many relationship for categories.
+        """
         return ", ".join([category.name for category in obj.categories.all()])
     get_categories.short_description = 'Categories' 
