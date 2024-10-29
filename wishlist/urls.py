6406ddc2f74a1a1a8b77da6e3a8632a5c115fe0a
Simon Moynihan <simon.moynihan33@gmail.com>
2024-10-29 14:40:56 +0000

    Add wishlist functionality to views and update wishlist button for wishlist html

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
          name='add_to_wishlist'),
     path('remove/<int:product_id>/', views.remove_from_wishlist,
          name='remove_from_wishlist'),
+    path('toggle_wishlist/<int:product_id>/', views.toggle_wishlist,
+         name='toggle_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Wishlist
 from products.models import Product
@@ -25,3 +25,16 @@ def remove_from_wishlist(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     Wishlist.objects.filter(user=request.user, product=product).delete()
     return redirect('wishlist')
+
+
+@login_required
+def toggle_wishlist(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    wishlist_item, created = Wishlist.objects.get_or_create(
+        user=request.user, product=product)
+    if not created:
+        # If the item was already in the wishlist, remove it
+        wishlist_item.delete()
+    return redirect(request.META.get(
+        'HTTP_REFERER', reverse('product_detail', args=[product.id])))
+
