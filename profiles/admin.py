fbf1cc40e336bae6beb8117117672cde5cd03b52
Simon Moynihan <simon.moynihan33@gmail.com>
2024-10-30 23:24:14 +0000

    Edit admin code to verify emails in django admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,16 +1,30 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin, User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
 from .models import CustomUser
+
+
+def verify_email(modeladmin, request, queryset):
+    for user in queryset:
+        email_address = EmailAddress.objects.filter(user=user).first()
+        if email_address:
+            email_address.verified = True
+            email_address.save()
+
+
+verify_email.short_description = "Mark selected users as email verified"
 
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'is_email_verified')
     readonly_fields = ('is_email_verified',)
+    actions = [verify_email]
 
     def is_email_verified(self, obj):
         """Check if the user’s email is verified."""
         return obj.is_email_verified()
-    is_email_verified.boolean = True  # Display as a boolean (checkbox)
+    is_email_verified.boolean = True
     is_email_verified.short_description = 'Email Verified'
 
 
