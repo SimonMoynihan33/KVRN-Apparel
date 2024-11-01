98178506e2a5d1bdfc87e1407222c42999cc86c2
Simon Moynihan <simon.moynihan33@gmail.com>
2024-11-01 14:33:53 +0000

    Add admin interface for contact form

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'submitted_at')
+    search_fields = ('name', 'email', 'subject')
+    list_filter = ('submitted_at',)  # Filter by submission date
+    readonly_fields = ('submitted_at',)
+
+    # Optional: Customize how the form appears in the admin detail view
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'email', 'subject', 'message', 'submitted_at')
+        }),
+    )
