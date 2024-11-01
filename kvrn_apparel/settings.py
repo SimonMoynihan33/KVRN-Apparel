11902f1a7c6e5b08bb001b744485301add3bb20c
Simon Moynihan <simon.moynihan33@gmail.com>
2024-11-01 13:22:30 +0000

    Email fix push

--- kvrn_apparel/settings.py
+++ kvrn_apparel/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'kvrn-apparel-4bc2d092ebd6.herokuapp.com',
@@ -223,7 +223,7 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
+    EMAIL_PORT = 465
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
