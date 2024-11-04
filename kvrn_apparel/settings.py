d822d8592bd96f1dec8548cf3437842e398fc8cf
Simon Moynihan <simon.moynihan33@gmail.com>
2024-11-04 18:37:38 +0000

    Set email to use SSL instead of TLS

--- kvrn_apparel/settings.py
+++ kvrn_apparel/settings.py
@@ -221,9 +221,9 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'kvrnapparel@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_USE_SSL = False
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+    EMAIL_PORT = 465
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
