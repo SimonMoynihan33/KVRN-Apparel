22e4bb1672d593a30c9941835427202a4cbb21bd
Simon Moynihan <simon.moynihan33@gmail.com>
2024-11-04 17:39:05 +0000

    Debugging live site

--- kvrn_apparel/settings.py
+++ kvrn_apparel/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'kvrn-apparel-4bc2d092ebd6.herokuapp.com',
