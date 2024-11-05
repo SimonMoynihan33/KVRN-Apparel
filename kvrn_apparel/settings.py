9a3b6fef6dd1434631e6d4ca2f905e4598d9f0b9
Simon Moynihan <simon.moynihan33@gmail.com>
2024-11-05 17:34:23 +0000

    Debigging live site memory issue

--- kvrn_apparel/settings.py
+++ kvrn_apparel/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'kvrn-apparel-4bc2d092ebd6.herokuapp.com',
