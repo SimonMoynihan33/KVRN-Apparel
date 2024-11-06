825462313df135029125d879bdd488c74d075777
Simon Moynihan <simon.moynihan33@gmail.com>
2024-11-06 02:51:55 +0000

    Fix error in view with required email field not allowing updating of form

--- design_submissions/views.py
+++ design_submissions/views.py
@@ -11,42 +11,59 @@ def submit_design(request):
     View to submit designs to be featured on a graphic apparel product
     """
     submission_id = request.POST.get('submission_id')
-    if request.method == 'POST':
-        if submission_id:
-            # Attempt to edit an existing submission
-            submission = get_object_or_404(
-                UserDesignSubmission, id=submission_id, user=request.user)
-
-            # Check if the submission is processing or finalized
-            if submission.status in ['processing', 'finalized']:
-                messages.error(
-                    request,
-                    "This submission is already being processed or finalized \
-                        and cannot be edited."
-                )
-
-                return redirect('submit_design')
-
-            form = UserDesignSubmissionForm(
-                request.POST, request.FILES, instance=submission)
-        else:
-            # Create a new submission if no ID provided
-            form = UserDesignSubmissionForm(request.POST, request.FILES)
-
-        if form.is_valid():
-            new_submission = form.save(commit=False)
-            new_submission.user = request.user
-            new_submission.save()
-            return redirect('submit_design')
-    else:
-        # Show an empty form for a new submission
-        form = UserDesignSubmissionForm()
-
-    # Display all submissions by the user
     user_submissions = UserDesignSubmission.objects.filter(user=request.user)
+
+    if submission_id:
+        submission = get_object_or_404(
+            UserDesignSubmission, id=submission_id, user=request.user)
+
+        # Prevent editing if the status is 'processing' or 'finalized'
+        if submission.status in ['processing', 'finalized']:
+            messages.warning(
+                request, "This submission cannot be edited as it is already \
+                    being processed or finalized.")
+            form = UserDesignSubmissionForm()
+        else:
+            if request.method == 'POST':
+                form = UserDesignSubmissionForm(
+                    request.POST, request.FILES, instance=submission)
+                form.fields.pop('email', None)
+
+                if form.is_valid():
+                    submission = form.save(commit=False)
+                    submission.email = submission.email  # Keep existing email
+                    submission.save()
+                    messages.success(
+                        request, "Your design submission has been successfully\
+                             updated!")
+                    form = UserDesignSubmissionForm()
+                else:
+                    messages.error(
+                        request, "There was an error updating your submission.\
+                             Please check the form.")
+            else:
+                form = UserDesignSubmissionForm(instance=submission)
+                form.fields.pop('email', None)  # Exclude email field from form
+    else:
+        if request.method == 'POST':
+            form = UserDesignSubmissionForm(request.POST, request.FILES)
+            if form.is_valid():
+                submission = form.save(commit=False)
+                submission.user = request.user
+                submission.save()
+                messages.success(
+                    request, "Your design submission has been successfully \
+                        created!")
+                form = UserDesignSubmissionForm()
+            else:
+                messages.error(request, "There was an error with your \
+                    submission. Please check the form.")
+        else:
+            form = UserDesignSubmissionForm()
+
     return render(request, 'design_submissions/submit_design.html', {
         'form': form,
-        'user_submissions': user_submissions,
+        'user_submissions': user_submissions
     })
 
 
